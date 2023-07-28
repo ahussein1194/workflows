2e0c720e91c191c258c0f3ec78afb6ea69735614
Ahmed Hussein <ahussein1194@gmail.com>
2023-07-28 18:20:55 +0300

    merge_2nd_3rd

---
+++ merge_2nd_3rd/2nd.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh -l 
+cd /afs/cern.ch/user/a/ahgit/cmssw/CMSSW_13_0_10/src/DQM/RPCMonitorModule/test/2nd_3rd/RPCMon
+cmsRun calculateEfficiency.py 370 753
--- merge_2nd_3rd/merge.sh
+++ merge_2nd_3rd/merge.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh -l 
 cd /eos/user/a/ahgit/rpc_workspace/runs_output/RPCMon/output_370753
-hadd -j 8 -f -O Efficiency_370580_RPCMon2023.root *.root
+hadd -j 8 -f -O Efficiency_370753_RPCMon2023.root *.root
+mv Efficiency_370753_RPCMon2023.root /afs/cern.ch/user/a/ahgit/cmssw/CMSSW_13_0_10/src/DQM/RPCMonitorModule/test/2nd_3rd/RPCMon
