51fc220287848bb52309b05708d82eb92e3b514c
Ahmed Hussein <ahussein1194@gmail.com>
2023-07-28 18:26:12 +0300

    merge_2nd_3rd

--- merge_2nd_3rd/2nd.sh
+++ merge_2nd_3rd/2nd.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh -l 
-cd /afs/cern.ch/user/a/ahgit/cmssw/CMSSW_13_0_10/src/DQM/RPCMonitorModule/test/2nd_3rd/RPCMon
+cd /afs/cern.ch/user/a/ahgit/cmssw/CMSSW_13_0_10/src
+cmsenv
+cd DQM/RPCMonitorModule/test/2nd_3rd/RPCMon
 cmsRun calculateEfficiency.py 370 753
