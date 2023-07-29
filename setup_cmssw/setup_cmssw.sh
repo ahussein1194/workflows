f0a0ed91db1926d74cc4238df40eb94482e61275
Ahmed Hussein <ahussein1194@gmail.com>
2023-07-30 00:20:14 +0300

    setup_cmssw

---
+++ setup_cmssw/setup_cmssw.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh -l 
+cd /afs/cern.ch/user/a/ahgit/cmssw
+cmsrel CMSSW_13_0_9
+cd CMSSW_13_0_9/src
+cmsenv
+source /cvmfs/cms.cern.ch/crab3/crab.sh
+git cms-addpkg RecoLocalMuon/RPCRecHit
+git clone -b run3_offlineAna_12_5_1 https://gitlab.cern.ch/CMSRPCDPG/OfflineRPCEfficiency.git
