5eab0e4020afe97df843be98206c6cfd7435d88d
Ahmed Hussein <ahussein1194@gmail.com>
2023-08-03 02:11:54 +0300

    1st-step

--- 1st-step/submit_condor.sh
+++ 1st-step/submit_condor.sh
@@ -8,4 +8,11 @@ grep -o -P '(?<=cluster ).*?(?=\.)' submit_result.txt > cluster_id.txt
 cluster_id=$(grep -o -P '(?<=cluster ).*?(?=\.)' submit_result.txt)
 condor_q $cluster_id > condor_query.txt
 jobs_remaining=$(grep -o -P '(?<=Total for query: ).*?(?= jobs)' condor_query.txt)
-echo "#Remaining jobs: " $jobs_remaining
+#echo "#Remaining jobs: " $jobs_remaining
+while [ "$jobs_remaining" -ne 0]; do
+	echo "#Remaining jobs: $jobs_remaining."
+	sleep 5
+	condor_q $cluster_id > condor_query.txt
+	jobs_remaining=$(grep -o -P '(?<=Total for query: ).*?(?= jobs)' condor_query.txt)
+done
+
