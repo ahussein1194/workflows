53d0409ce31b35ba726fc3741a153a4f7a4c76f7
Ahmed Hussein <ahussein1194@gmail.com>
2023-08-03 03:26:01 +0300

    1st-step

--- 1st-step/submit_condor.sh
+++ 1st-step/submit_condor.sh
@@ -4,15 +4,14 @@ cmsenv
 cd DQM/RPCMonitorModule/test/condor/Express_Cosmics
 ./submit_condor.sh 371 208 > submit_result.txt
 cat submit_result.txt
-grep -o -P '(?<=cluster ).*?(?=\.)' submit_result.txt > cluster_id.txt
 cluster_id=$(grep -o -P '(?<=cluster ).*?(?=\.)' submit_result.txt)
 condor_q $cluster_id > condor_query.txt
 jobs_remaining=$(grep -o -P '(?<=Total for query: ).*?(?= jobs)' condor_query.txt)
-#echo "#Remaining jobs: " $jobs_remaining
 while [ "$jobs_remaining" -ne 0 ]; do
-	echo "#Remaining jobs: $jobs_remaining."
 	sleep 5
 	condor_q $cluster_id > condor_query.txt
+	cat condor_query.txt
 	jobs_remaining=$(grep -o -P '(?<=Total for query: ).*?(?= jobs)' condor_query.txt)
 done
-
+rm -rf submit_result.txt
+rm -rf condor_query.txt
