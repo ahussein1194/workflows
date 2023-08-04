d1726c9ef3aec85b9ec9ab1b42f311731995d087
Ahmed Hussein <ahussein1194@gmail.com>
2023-08-04 16:38:00 +0300

    1st-step

--- 1st-step/submit_condor.sh
+++ 1st-step/submit_condor.sh
@@ -15,6 +15,7 @@ while [ "$resubmit_to_condor" = true ]; do
 	if [ "$cluster_id" = "" ]; then
 		rm -rf submit_result.txt
 		echo "Submission failed. No jobs were submitted!"
+		mattermostSend(color: "danger", message: "Build FAILED: ${env.JOB_NAME} #${env.BUILD_DISPLAY_NAME} (<${env.BUILD_URL}|Link to build>)")
 		exit 1
 	fi
 	# Get the number of submitted jobs.
