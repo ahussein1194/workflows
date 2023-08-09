96a4777b1574f679cf81734e24001a4b7e79d125
Ahmed Hussein <ahussein1194@gmail.com>
2023-08-10 01:02:10 +0300

    SegmentExtrapolation-test

---
+++ plotNpublish/plotNpublish.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh -l 
+cd /afs/cern.ch/user/a/ahgit/cmssw/CMSSW_13_0_10/src/DQM/RPCMonitorModule/macros
+mkdir plots_${1}${2}
+cd plots_${1}${2}
+cp -r ../Chamber_Issues .
+cp ../Construct_bChambers.py .
+cp ../CSVReader.h .
+cp ../EndcapPlot.C .
+cp ../WheelPlot.C .
+cp ../drawEfficiencyHistograms.C .
+cp /afs/cern.ch/user/a/ahgit/cmssw/CMSSW_13_0_10/src/DQM/RPCMonitorModule/test/condor/Express_Cosmics/run_${1}${2}/output_${1}${2}/3steps/SummaryAnalyzeEfficiency_${1}${2}_Express2023.root .
+python3 Construct_bChambers.py
+root -l -b -q 'drawEfficiencyHistograms.C(true, true, 1, "SummaryAnalyzeEfficiency_${1}${2}_Express2023.root")'
