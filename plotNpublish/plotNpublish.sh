983a4ecbca70594f34373ffda665df48750e7056
Ahmed Hussein <ahussein1194@gmail.com>
2023-08-10 01:37:00 +0300

    Plot and Publish

--- plotNpublish/plotNpublish.sh
+++ plotNpublish/plotNpublish.sh
@@ -12,7 +12,7 @@ cp ../drawEfficiencyHistograms.C .
 cp /afs/cern.ch/user/a/ahgit/cmssw/CMSSW_13_0_10/src/DQM/RPCMonitorModule/test/condor/Express_Cosmics/run_${1}${2}/output_${1}${2}/3steps/SummaryAnalyzeEfficiency_${1}${2}_Express2023.root .
 python3 Construct_bChambers.py
 summaryFile_name="SummaryAnalyzeEfficiency_${1}${2}_Express2023.root"
-root -l -b -q "drawEfficiencyHistograms.C(true, true, 1, \"$summaryFile_name\")"
+root -l -b -q "drawEfficiencyHistograms.C(true, false, 1, \"$summaryFile_name\")"
 mkdir /eos/user/a/ahgit/rpc_workspace/plots/run_${1}${2}
 cp *.png /eos/user/a/ahgit/rpc_workspace/plots/run_${1}${2}
 cd ../
