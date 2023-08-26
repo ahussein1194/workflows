e3b5335993acf9fef516731caefd8152aca6557f
Ahmed Hussein <ahussein1194@gmail.com>
2023-08-26 11:45:36 +0300

    setup cmssw

--- setup_cmssw/setup_cmssw.sh
+++ setup_cmssw/setup_cmssw.sh
@@ -6,4 +6,5 @@ cd CMSSW_13_0_9/src
 cmsenv
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 git cms-addpkg RecoLocalMuon/RPCRecHit
-git clone -b run3_offlineAna_12_5_1 https://gitlab.cern.ch/CMSRPCDPG/OfflineRPCEfficiency.git -c http.extraheader="PRIVATE-TOKEN: $GITLAB_TOKEN" 
+#git clone -b run3_offlineAna_12_5_1 https://gitlab.cern.ch/CMSRPCDPG/OfflineRPCEfficiency.git 
+ssh-agent bash -c 'ssh-add ${sshKeyCredentialId} && git clone -b run3_offlineAna_12_5_1 https://gitlab.cern.ch/CMSRPCDPG/OfflineRPCEfficiency.git'
